e9ec8c15b4847ad9b05009964d710389bee8ea4c
ducknoir <ducknoir@users.noreply.github.com>
2024-11-24 15:33:37 -0800

    Recreate Jest configuration for local unit tests, using ES6 modules.

---
+++ jest.config.mjs
@@ -0,0 +1,6 @@
+export default {
+    testEnvironment: "jsdom", // Simulates a browser environment for DOM-related tests
+    roots: ["<rootDir>/docs"], // Limits tests to files in the 'docs' folder
+    moduleFileExtensions: ["js", "mjs"], // Supports ES6 module extensions
+  };
+  
