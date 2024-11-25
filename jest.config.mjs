c820ee807c9b08d782de839fe4739980d5b7546f
ducknoir <ducknoir@users.noreply.github.com>
2024-11-24 16:34:27 -0800

    Change Jest config to use __tests__ folder in project root.

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,6 @@
 export default {
     testEnvironment: "jsdom", // Simulates a browser environment for DOM-related tests
-    roots: ["<rootDir>/docs"], // Limits tests to files in the 'docs' folder
+    roots: ["<rootDir>/__tests__"], // Limits tests to files in the 'docs' folder
     moduleFileExtensions: ["js", "mjs"], // Supports ES6 module extensions
   };
   
